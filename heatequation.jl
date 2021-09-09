f9288bbf5822a3a4f0a8edc82dccb09e1290b4b8
minwoos2 <73131628+minwoos2@users.noreply.github.com>
2021-09-09 17:50:13 -0500

    second update : added emission

--- heatequation.jl
+++ heatequation.jl
@@ -14,13 +14,28 @@ x_max = 2.
 y_min = 0.
 y_max = 2.
 
+#xmax = 50.0
+#ymax = 30.0
+#ncells = 50.0
+#dx = x_max / ncells
+
+
+lecturer(x, y) = x > x_max / 2 - dx && x < x_max / 2 + dx && y > y_max / 10 - dx && y < y_max / 10 + dx
+emission(x, y, emisrate) = lecturer(x, y) ? emisrate : 0
+@register emission(x, y, emisrate)
+emisrate = 10;
+
+
+
 # 3D PDE
-eq  = Dt(u(t,x,y)) ~ Dxx(u(t,x,y)) + Dyy(u(t,x,y))
+eq  = Dt(u(t,x,y)) ~ Dxx(u(t,x,y)) + Dyy(u(t,x,y)) + emission(x, y, emisrate)
+#emission(x, y, emisrate) = emisrate
 
 analytic_sol_func(t,x,y) = exp(x+y)*cos(x+y+4t)
 # Initial and boundary conditionn
 
-bcs = [u(t_min,x,y) ~ analytic_sol_func(t_min,x,y),
+bcs = #[u(t_min,x,y) ~ analytic_sol_func(t_min,x,y),
+      [u(t_min,x,y) ~ 0,
        u(t,x_min,y) ~ 0,
        u(t,x_max,y) ~ 0,
        u(t,x,y_min) ~ 0,
@@ -41,7 +56,7 @@ sol = solve(prob,Tsit5())
 # Ploting
 using Plots
 xs,ys = [infimum(d.domain):dx:supremum(d.domain) for d in domains]
-u_sol = reshape(sol.u[100], length(xs)-2,length(ys)-2)
+u_sol = reshape(sol.u[300], length(xs)-2,length(ys)-2)
 
 plot(xs[2:length(xs)-1], ys[2:length(ys)-1], u_sol, linetype=:contourf,title = "solution")
 
